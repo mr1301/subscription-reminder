57101eff32edb56b7c86eb67dc1f7c7f869b1007
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-04-30 21:22:14 -0400

    Import packages from test file

--- subscription.py
+++ subscription.py
@@ -1,1 +1,16 @@
-Add my code here
+import email
+import imaplib
+from email.message import EmailMessage
+import mimetypes
+from email import header
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
+from datetime import timedelta
+import email.utils
+import time
+from email.utils import parsedate_to_datetime
+from email.utils import parsedate_tz
+import datetime
+import config
