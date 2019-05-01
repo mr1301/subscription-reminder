163e7f3a2b259539b375698d6d375e0320db3540
mr1301 <46725466+mr1301@users.noreply.github.com>
2019-04-30 21:23:00 -0400

    Update subscription.py

--- subscription.py
+++ subscription.py
@@ -14,3 +14,11 @@ from email.utils import parsedate_to_datetime
 from email.utils import parsedate_tz
 import datetime
 import config
+
+#==============Define Functions to be Used=============================
+#Getting message body
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else: 
+        return msg.get_payload(None,True)
